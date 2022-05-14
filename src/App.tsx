bd4b9e8037a5b90b70ae08cae86c582976fb8f46
ceyhan bekiroglu <ceyhanbekiroglu@gmail.com>
2022-05-14 18:01:15 +0200

    Add Item map

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,7 @@ import LinererProgress from '@material-ui/core/LinearProgress'
 import Grid from '@material-ui/core/Grid'
 import AddShoppingCartIcon from '@material-ui/icons/AddShoppingCart'
 import Badge from '@material-ui/core/Badge'
+import Item from './Item/Item'
 
 import { Wrapper } from './App.styles'
 import LinearProgress from '@material-ui/core/LinearProgress'
@@ -37,7 +38,17 @@ function App() {
 
 	console.log(data)
 
-	return <div className='App'>Start</div>
+	return (
+		<Wrapper>
+			<Grid container spacing={3}>
+				{data?.map((item) => (
+					<Grid item key={item.id} xs={12} sm={4}>
+						<Item item={item} handleAddToCart={handleAddToCart} />
+					</Grid>
+				))}
+			</Grid>
+		</Wrapper>
+	)
 }
 
 export default App
