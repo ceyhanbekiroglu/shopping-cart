cd130905885e1091dfc4a8fe8db847984c298760
ceyhan bekiroglu <ceyhanbekiroglu@gmail.com>
2022-05-21 21:57:47 +0200

    Add `CartItem` function

--- src/CartItem/CartItem.tsx
+++ src/CartItem/CartItem.tsx
@@ -3,3 +3,7 @@ import Button from '@material-ui/core/Button'
 import { CartItemType } from '../App'
 
 import { Wrapper } from './CartItem.style'
+
+const CartItem: React.FC = () => <div>Cart Item</div>
+
+export default CartItem
