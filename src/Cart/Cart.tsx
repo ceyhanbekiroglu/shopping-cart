bd995b9d52ac23f893217bbf036e5af6da4390ce
ceyhan bekiroglu <ceyhanbekiroglu@gmail.com>
2022-05-21 22:03:19 +0200

    Finish `Cart` function

--- src/Cart/Cart.tsx
+++ src/Cart/Cart.tsx
@@ -10,4 +10,16 @@ type Props = {
 	removeFromCart: (id: number) => void
 }
 
-const Cart: React.FC<Props> = ({ cartItems, addToCart, removeFromCart }) => {}
+const Cart: React.FC<Props> = ({ cartItems, addToCart, removeFromCart }) => {
+	return (
+		<Wrapper>
+			<h2> Your Shopping Cart</h2>
+			{cartItems.length === 0 ? <p> No items in the cart.</p> : null}
+			{cartItems.map((item) => (
+				<CartItem />
+			))}
+		</Wrapper>
+	)
+}
+
+export default Cart
