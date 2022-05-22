1a979437e66e7218c52d4c84809458ae97191b7f
ceyhan bekiroglu <ceyhanbekiroglu@gmail.com>
2022-05-22 22:33:53 +0200

    Add buttons to Cart

--- src/CartItem/CartItem.tsx
+++ src/CartItem/CartItem.tsx
@@ -19,6 +19,25 @@ const CartItem: React.FC<Props> = ({ item, addToCart, removeFromCart }) => (
 				<p>Price: €{item.price}</p>
 				<p>Total: €{(item.amount * item.price).toFixed(2)}</p>
 			</div>
+			<div className='buttons'>
+				<Button
+					size='small'
+					disableElevation
+					variant='contained'
+					onClick={() => removeFromCart(item.id)}
+				>
+					-
+				</Button>
+				<p>{item.amount}</p>
+				<Button
+					size='small'
+					disableElevation
+					variant='contained'
+					onClick={() => addToCart(item)}
+				>
+					+
+				</Button>
+			</div>
 		</div>
 	</Wrapper>
 )
