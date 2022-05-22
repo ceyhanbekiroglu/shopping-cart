54b8b1fe47e4e61b12c97ff60f0442d792e4840e
ceyhan bekiroglu <ceyhanbekiroglu@gmail.com>
2022-05-22 22:30:58 +0200

    Add Title, price and total to Cart

--- src/CartItem/CartItem.tsx
+++ src/CartItem/CartItem.tsx
@@ -15,6 +15,10 @@ const CartItem: React.FC<Props> = ({ item, addToCart, removeFromCart }) => (
 	<Wrapper>
 		<div>
 			<h3> {item.title}</h3>
+			<div className='information'>
+				<p>Price: €{item.price}</p>
+				<p>Total: €{(item.amount * item.price).toFixed(2)}</p>
+			</div>
 		</div>
 	</Wrapper>
 )
